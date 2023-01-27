38dd522cb2340d704ab67cdd65af55a4ec2a3f5d
Danila Belitsky <belitsky.danila@gmail.com>
2023-01-27 10:47:59 +0300

    Complete Hometask-3

---
+++ Hometasks/Hometask3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Hometask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputNumbers();
+            Console.WriteLine();
+
+            CounterLoop();
+            Console.WriteLine('\n');
+
+            ArithmeticMean(1, 3);
+        }
+
+        private static void OutputNumbers()
+        {
+            int number = -20;
+
+            do
+            {
+                Console.WriteLine(number);
+                number -= 20;
+            } while (number >= -100);
+        }
+
+        private static void CounterLoop()
+        {
+            for (int i = 10; i < 100; i += 5)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        private static void ArithmeticMean(int a, int b)
+        {
+            int sum = 0, counter = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+                counter++;
+            }
+
+            Console.WriteLine($"Sum = {sum}, Arithmetic mean = {sum/counter}");
+        }
+    }
+}
