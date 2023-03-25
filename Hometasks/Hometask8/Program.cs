0f7d3dcdd635e9dd20ddd92666788b80b8662739
Danila Belitsky <belitsky.danila@gmail.com>
2023-03-25 12:34:58 +0300

    Complete task 1

---
+++ Hometasks/Hometask8/Program.cs
@@ -0,0 +1,14 @@
+﻿using Hometask8.Hometask8_1;
+
+namespace Hometask8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Explorer explorer = new Explorer();
+
+            explorer.Manage();
+        }
+    }
+}
