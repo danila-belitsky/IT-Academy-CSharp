fda7fccda40f43298b24452adff76122927cb3f5
Danila Belitsky <belitsky.danila@gmail.com>
2023-03-25 13:23:49 +0300

    Complete task 2

--- Hometasks/Hometask8/Program.cs
+++ Hometasks/Hometask8/Program.cs
@@ -1,4 +1,5 @@
-﻿using Hometask8.Hometask8_1;
+﻿using dz8_namespace;
+using System.Reflection;
 
 namespace Hometask8
 {
@@ -6,9 +7,24 @@ namespace Hometask8
     {
         static void Main(string[] args)
         {
-            Explorer explorer = new Explorer();
+            // Explorer explorer = new Explorer();
 
-            explorer.Manage();
+            // explorer.Manage();
+
+            Type type = typeof(Class_dz8);
+
+            Console.WriteLine(type);
+            Console.WriteLine($"Name: {type.Name}");
+            Console.WriteLine($"Full name: {type.FullName}");
+            Console.WriteLine($"Namespace: {type.Namespace}");
+            Console.WriteLine($"Is class: {type.IsClass}\n");
+
+            foreach (MemberInfo member in type.GetMembers())
+            {
+                Console.WriteLine($"{member.DeclaringType} {member.MemberType} {member.Name}");
+            }
+
+            Class_dz8 class_Dz8 = new Class_dz8();
         }
     }
 }
